12b82152677e895dd0aa9ae1b26353bf3fbc1aed
EJ05 <eejoylim@gmail.com>
2023-12-25 12:44:45 -0500

    Clean up previous showModal code

--- views/networks/networks.js
+++ views/networks/networks.js
@@ -560,41 +560,6 @@ const initGridButton = function() {
     return true
 }
 
-const showModal = function(network) {
-    var modal = document.getElementById("modal");
-    var btn = document.getElementById("info-button");
-    var span = document.getElementsByClassName("close")[0];
-
-    if (network.name) $("#nameVal").text(network.name);
-    else $("#nameVal").text("No name for this network.");
-    
-    if (network.description) $("#descVal").text(network.description);
-    else $("#descVal").text("No description for this network.");
-
-    if (network.author) $("#authorVal").text(network.author);
-    else $("#authorVal").text("No author for this network.");
-
-    if (network.nodes.length) $("#nodesVal").text(network.nodes.length);
-    else $("#nodesVal").text("No nodes for this network.");
-
-    if (network.edges.length) $("#edgesVal").text(network.edges.length);
-    else $("#edgesVal").text("No edges for this network.");
-
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-}
-
 //View Controller and Init Function
 const ViewController = {
     view: "",
